4ba7ab04a5557bdeea7fc17fe63220d1bdfc4bea
Alan Gabriel Dias Pedon <alangabrieldias@hotmail.com>
2024-04-25 16:40:35 -0300

    WIK: User and Company Register

--- src/controllers/page.controller.ts
+++ src/controllers/page.controller.ts
@@ -1,6 +1,6 @@
 import { RequestHandler } from 'express';
 
 export const home: RequestHandler = async (req, res) => {
-  return res.render("./includes/Navbar.ejs");
+  return res.render("index.ejs");
 };
 
